0030eded9c698a7859036c33c34b7cd9243bc802
岩崎　有輝 <yuki@iwayukinoMacBook-Pro.local>
2026-06-15 11:48:35 +0900

    login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
+    skip_before_action :require_login,only:%i[home]
+
+  def home ; end
+  
    layout "application"
     def index
     end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+   has_secure_password
+    validates :password, length: {minimum:5}, if: -> {new_record? || changes[:password_digest]}
+    validates :name,presence: true,length: {maximum:100} unipuness: true
+    validates :email,presence: true, unipuness: true
+
     has_many :boards, dependent: :destroy
     
 end
