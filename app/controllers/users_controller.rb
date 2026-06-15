260b704250c4c53e0cd25df10a14807f684ca5d4
岩崎　有輝 <yuki@iwayukinoMacBook-Pro.local>
2026-06-15 17:23:31 +0900

    flashmove

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-        redirect_to root_path
+        redirect_to root_path,success:'ユーザー登録完了！'
     else
-        render :new
+        flash.now[:danger] ='ユーザー登録に失敗'
+        render :new,status: :unprocessable_entity
     end
   end
 
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,22 @@
 // Entry point for the build script in your package.json
 //import "@hotwired/turbo-rails"
+
 import "./controllers"
 import * as bootstrap from "bootstrap"
 
 window.bootstrap = bootstrap
 
 import "bootstrap"
+
+function removeFlash() {
+  const flashMessages = document.querySelectorAll(".flash-message");
+
+  flashMessages.forEach((message) => {
+    setTimeout(() => {
+      message.remove();
+    }, 3000);
+  });
+}
+
+document.addEventListener("turbo:load", removeFlash);
+document.addEventListener("DOMContentLoaded", removeFlash);
