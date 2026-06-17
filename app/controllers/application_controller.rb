95f81d6fdb212d2ed38c15f99594f44e9bfc094e
ユウキ <yuki.iwasaki121@gmail.com>
2026-06-17 11:28:28 +0900

    midasi

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user = current_user&.decorate
+  @current_user ||= User.find_by(id: session[:user_id])&.decorate
   end
 
   private
