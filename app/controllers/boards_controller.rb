533ac38e14554c94eff634f295f8eaa7e7988c19
ユウキ <yuki.iwasaki121@gmail.com>
2026-06-17 17:12:24 +0900

    picture

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -21,7 +21,7 @@ class BoardsController < ApplicationController
  private
 
  def board_params
-   params.require(:board).permit(:title, :body)
+   params.require(:board).permit(:title, :body, :board_image, :board_image_cache)
  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_06_15_022532) do
+ActiveRecord::Schema[7.2].define(version: 2026_06_17_074808) do
   create_table "boards", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "board_image"
     t.index ["user_id"], name: "index_boards_on_user_id"
   end
 
