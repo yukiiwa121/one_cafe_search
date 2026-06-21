2a7f5c7e3aa8b312359661958bb2abc34b093b8b
ユウキ <yuki.iwasaki121@gmail.com>
2026-06-21 17:43:26 +0900

    javaclear

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,16 @@
 require_relative "boot"
 
 require "rails"
+require "active_model/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "action_mailer/railtie"
+require "active_job/railtie"
+require "action_cable/railtie"
+require "rails/test_unit/railtie"
+# sprocketsを完全無効化
+# require "sprockets/railtie" ← もしあれば削除
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
