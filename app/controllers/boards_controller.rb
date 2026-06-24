62abbe43395290deaef60e2cb79b65b63c8b83a5
ユウキ <yuki.iwasaki121@gmail.com>
2026-06-24 11:18:51 +0900

    beforelogin

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,6 @@
 class BoardsController < ApplicationController
-    
+  skip_before_action :require_login,only: %i[index]  
+
  def index
     @boards = Board.includes(:user).order(created_at: :desc)
  end
