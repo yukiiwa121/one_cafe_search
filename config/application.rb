d52bcdf8174e48a29cb5e8e37c1252ef76a7b9b0
ユウキ <yuki.iwasaki121@gmail.com>
2026-06-24 10:14:40 +0900

    webrestart

--- config/application.rb
+++ config/application.rb
@@ -7,8 +7,8 @@ require "action_controller/railtie"
 require "action_view/railtie"
 require "action_mailer/railtie"
 require "active_job/railtie"
-require "action_cable/railtie"
-require "rails/test_unit/railtie"
+require "sprockets/railtie"
+require "active_storage/engine"
 # sprocketsを完全無効化
 # require "sprockets/railtie" ← もしあれば削除
 
